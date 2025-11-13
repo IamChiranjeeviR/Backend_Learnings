72a8d8e4624ae97b4510e19ddea970d7720bb028
Chiranjeevi <iamchiranjeeviramakrishna@gmail.com>
2025-11-13 07:54:31 +0530

    Added Login, Logout, Register & auth APIs

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import "dotenv/config";
+import connectDb from "./configs/db.js";
+import userRouter from "./routes/userRouter.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+await connectDb();
 
 const allowedOrigins = [""];
 
@@ -15,6 +20,8 @@ app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.get("/", (req, res) => {
   res.send("Api is working");
 });
+
+app.use("/api/user", userRouter);
 
 app.listen(port, () => {
   console.log(`server is running on http://localhost:${port}`);
